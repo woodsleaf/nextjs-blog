1ba974dd74abbdd1e827f042abddd83d9273927b
Pavel Kononov <woodsleaf@gmail.com>
2021-04-22 21:05:11 +0300

    Pre DinamicRoutes

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import utilStyles from '../../styles/utils.module.css'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+//for formatting date
+import Date from '../../components/date'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        {/*postData.id*/}
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
