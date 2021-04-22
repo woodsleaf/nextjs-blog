45736f0f53335a36a03c1a9d3080d4881d65e4cd
Pavel Kononov <woodsleaf@gmail.com>
2021-04-22 23:35:48 +0300

    api add hello

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+// pages/404.js
+import LayoutError from '../components/layouterror'
+
+export default function Custom404() {
+  return (
+    <LayoutError>
+      <h1>404 - Page Not Found</h1>
+    </LayoutError>
+  )
+}
